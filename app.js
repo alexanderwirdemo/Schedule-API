7ab76db41d63be05da2d67246c7236f7da4a337c
Kikachy <82896550+Kikachy@users.noreply.github.com>
2021-11-24 22:03:32 +0100

    Login

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require('express-session');
-
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+
+const DATABASE = 'Schedule'
+const DB_USER = 'lidiya' // Database connection user
+const DB_PASSWORD = 'dvpMXNSQWFAp2UGb' // Database connection password
+const DATABASE_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@ltu-rest-ws.mwzyn.mongodb.net/test?authSource=admin&replicaSet=atlas-i5ooth-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`;
 
 // Läs in Schemana
 //var Student = require("./models/student.js");
@@ -47,7 +52,16 @@ app.all('/*', function(req, res, next) {
   next();
 });*/
 
-// Starta servern
-app.listen(port, function() {
-    console.log("Server running on port " + port);
+// Create a MongoDB connection pool and start the application
+// after the database connection is ready
+MongoClient.connect(DATABASE_URL, (err, db) => {
+    if (err) {
+        console.error(`Failed to connect to the database. ${err.stack}`);
+    }
+    app.locals.db = db.db(DATABASE) // Connect to database "Schedule"
+
+    // Starta servern
+    app.listen(port, function() {
+        console.log("Server running on port " + port);
+    });
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,26 @@
 module.exports = function(app) {
-    app.post("/login", function(req, res) {
-        let email = req.body.email
-        let password = req.body.password
 
-        console.log(email, password)
-        if (email == 'correct@example.com' && password == 'password') {
-            console.log('Logged in!')
-                // TODO: Store information and log in
-            res.status(200).send();
-        } else {
-            console.log('Wrong information!')
-            res.status(403).send()
-        }
+    app.post("/login", function(req, res) {
+        let db = req.app.locals.db // Database connection stored in app
+        let email = req.body.email // Email that was entered
+        let password = req.body.password // Password that was entered
+
+        // Query database for user and password
+        db.collection('Schedule-Frontend').find({ "E-mail": email, "Password": password }).toArray().then((results) => {
+            results.forEach((result) => {
+                if (result['E-mail'] === email && result['Password'] === password) {
+                    // TODO: Actually log in user
+                    res.status(200).send();
+                    return;
+                }
+            })
+            res.status(403).send();
+        });
     });
 
     app.post("/logout", function(req, res) {
-        console.log('Logged out in backend')
+        console.log('Logged out in backend');
+        // TODO: Actually log out user
         res.status(200).send(json);
     });
 }
